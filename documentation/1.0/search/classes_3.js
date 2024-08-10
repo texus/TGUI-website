ecd8c5d395421008b6b34f4ad0f6691079baa18b
Bruno Van de Velde <vdv_b@tgui.eu>
2024-08-10 19:39:56 +0200

    Update documentation (doxygen-awesome updated to 2.3.3 + got rid of double scrollbar + some functions that will be part of TGUI 1.5 were already added)

--- documentation/1.0/search/classes_3.js
+++ documentation/1.0/search/classes_3.js
@@ -3,5 +3,6 @@ var searchData=
   ['defaultbackendwindow_0',['DefaultBackendWindow',['../classtgui_1_1DefaultBackendWindow.html',1,'tgui']]],
   ['defaultthemeloader_1',['DefaultThemeLoader',['../classtgui_1_1DefaultThemeLoader.html',1,'tgui']]],
   ['deserializer_2',['Deserializer',['../classtgui_1_1Deserializer.html',1,'tgui']]],
-  ['duration_3',['Duration',['../classtgui_1_1Duration.html',1,'tgui']]]
+  ['dualscrollbarchildinterface_3',['DualScrollbarChildInterface',['../classtgui_1_1DualScrollbarChildInterface.html',1,'tgui']]],
+  ['duration_4',['Duration',['../classtgui_1_1Duration.html',1,'tgui']]]
 ];
